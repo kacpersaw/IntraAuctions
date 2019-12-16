98c70119c254c5b616e917db6afa4f1d97d3ae86
Kacper Sawicki <kacper@ksawicki.pl>
2019-12-16 20:32:07 +0100

    Changed db driver to mysql

--- server/cmd/server.go
+++ server/cmd/server.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"gitlab.com/kacpersaw/intra-auctions/model"
+	"gitlab.com/kacpersaw/intra-auctions/router"
+	"net/http"
 )
 
 func init() {
@@ -14,15 +18,15 @@ var serverCmd = &cobra.Command{
 	Long:  "",
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		//model.DB = model.InitDB()
-		//
-		//logrus.Info("Starting API on port :" + args[0])
-		//r := router.NewRouter()
-		//logrus.Fatal(http.ListenAndServe(
-		//	":"+args[0],
-		//	router.CommonMiddleware(
-		//		router.CorsMiddleware(r),
-		//	),
-		//))
+		model.DB = model.InitDB()
+
+		logrus.Info("Starting API on port :" + args[0])
+		r := router.NewRouter()
+		logrus.Fatal(http.ListenAndServe(
+			":"+args[0],
+			router.CommonMiddleware(
+				router.CorsMiddleware(r),
+			),
+		))
 	},
 }
--- server/model/model.go
+++ server/model/model.go
@@ -1,9 +1,11 @@
 package model
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/sirupsen/logrus"
+	"gitlab.com/kacpersaw/intra-auctions/config"
 )
 
 var (
@@ -11,7 +13,7 @@ var (
 )
 
 func InitDB() *gorm.DB {
-	db, err := gorm.Open("sqlite3", "intra-auctions.db")
+	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True", config.DB_USERNAME, config.DB_PASSWORD, config.DB_HOST, config.DB_PORT, config.DB_NAME))
 	if err != nil {
 		logrus.Error(err.Error())
 		panic("Faield to connect to database")
